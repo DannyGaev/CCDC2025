0aa75fb77e03c82ee85bca43d5a3a534ca1285b2
Daniel Gaevskiy <thefirstoaktree@gmail.com>
2025-02-11 00:48:45 -0600

    Included safeguard against changing 'promon' user password, as per CCDC regulations

--- auto_password_change/change_user_passwds.sh
+++ auto_password_change/change_user_passwds.sh
@@ -3,7 +3,7 @@
 # Step through all users on the system
 getent passwd | while IFS=: read -r name password uid gid gecos home shell; do
 	# If the user's uid is greater than 1000, change their password
-	if [[ "$uid" -ge 1000 || "$name" == "root" ]]; then
+	if [[ ( "$uid" -ge 1000 || "$name" == "root" ) && ( "$name" != "promon" ) ]]; then
 		./gen_and_update.sh $name
 	fi
 done
