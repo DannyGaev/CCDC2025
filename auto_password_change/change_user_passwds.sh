bb6c1d97c5ce47b448be814f136cc0c3186ab83b
Daniel Gaevskiy <thefirstoaktree@gmail.com>
2025-02-12 00:26:56 -0600

    Updated script to exclude user 'nobody' from password changes

--- auto_password_change/change_user_passwds.sh
+++ auto_password_change/change_user_passwds.sh
@@ -3,7 +3,7 @@
 # Step through all users on the system
 getent passwd | while IFS=: read -r name password uid gid gecos home shell; do
 	# If the user's uid is greater than 1000 (or are root) and they are not promon, change their password
-	if [[ ( "$uid" -ge 1000 || "$name" == "root" ) && ( "$name" != "promon" ) ]]; then
+	if [[ ( "$uid" -ge 1000 || "$name" == "root" ) && ( "$name" != "promon" && "$name" != "nobody" ) ]]; then
 		./gen_and_update.sh $name
 	fi
 done
