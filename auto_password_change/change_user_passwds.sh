16076276b548fd2fac3e16b37ce6104bbec9a150
Daniel Gaevskiy <thefirstoaktree@gmail.com>
2025-02-12 00:27:50 -0600

    Updated script to include a check to ensure it is being run as root

--- auto_password_change/change_user_passwds.sh
+++ auto_password_change/change_user_passwds.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/bash
 
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
 # Step through all users on the system
 getent passwd | while IFS=: read -r name password uid gid gecos home shell; do
 	# If the user's uid is greater than 1000 (or are root) and they are not promon, change their password
